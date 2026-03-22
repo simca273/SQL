6a50482e82ee55f8e6b2df08af44b6bb3f14ef26
simca273 <simca273@gmail.com>
2026-03-22 19:47:07 +0100

    Change query to select top-paying Data Scientist jobs

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,10 +1,3 @@
-/*
-Question: What are the top-paying data analyst jobs?
-- Identify the top 10 highest-paying Data Analyst roles that are available remotely.
-- Focuses on job postings with specified salaries (remove nulls).
-- Why? Highlight the top-paying opportunities for Data Analysts, offering insights into emp
-*/
-
 SELECT
     job_id,
     job_title,
@@ -18,7 +11,7 @@ FROM
 LEFT JOIN company_dim2
     ON job_postings_fact2.company_id = company_dim2.company_id
 WHERE
-    job_title_short = 'Data Analyst' AND
+    job_title_short = 'Data Scientist' AND
     job_location = 'Anywhere' AND
     salary_year_avg IS NOT NULL
 ORDER BY
