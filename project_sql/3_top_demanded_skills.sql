7e981ef82a279e331465d2a27ba300fe41495fdc
simca273 <simca273@gmail.com>
2026-03-22 19:48:27 +0100

    Refactor SQL query for top demanded skills and jobs

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -1,12 +1,3 @@
-/*
-Question: What are the most in_demand skills for data analysts?
-- Join job postings to inner join table similar to query 2.
-- Identify the top 5 in-demand skills for adata analyst.
-- Focus on all job postings.
-- Why? Retrieves the top 5 skills with the highest demand in the job market, 
-providing insights into the most valuable skills for job seekers.
-*/
-
 SELECT 
   skills,
     COUNT(skills_job_dim2.job_id) AS demand_count
@@ -35,7 +26,7 @@ WITH top_paying_jobs AS (
     LEFT JOIN company_dim2
         ON job_postings_fact2.company_id = company_dim2.company_id
     WHERE
-        job_title_short = 'Data Analyst' AND
+        job_title_short = 'Data Scientist' AND
         job_location = 'Anywhere' AND
         salary_year_avg IS NOT NULL
     ORDER BY
