64c835efe2006cc613389db64a8d0436f97e2cdb
simca273 <simca273@gmail.com>
2026-03-22 19:47:39 +0100

    Change job title from 'Data Analyst' to 'Data Scientist'

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -1,12 +1,3 @@
-/*
-Question: What skills are required for the top-paying data analyst jobs?
-- Use the specific skills required for these roles
-- Add the specific skills required for these roles
-- Why? It provides a detailed look at which high-paying jobs remand certain skills, 
-  helping job seekers understand which skills to develop that aligh with top salaries.
-  */
-
-
 WITH top_paying_jobs AS (
     SELECT
         job_id,
@@ -18,7 +9,7 @@ WITH top_paying_jobs AS (
     LEFT JOIN company_dim2
         ON job_postings_fact2.company_id = company_dim2.company_id
     WHERE
-        job_title_short = 'Data Analyst' AND
+        job_title_short = 'Data Scientist' AND
         job_location = 'Anywhere' AND
         salary_year_avg IS NOT NULL
     ORDER BY
